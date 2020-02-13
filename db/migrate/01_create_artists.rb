383c5ba237c40e0ab65764d477d035c5925dffa1
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-02-13 23:15:58 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,8 +1,11 @@
 class CreateArtists < ActiveRecord::Migration[5.2]
-  def up
-  end
- 
-  def down
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
 
