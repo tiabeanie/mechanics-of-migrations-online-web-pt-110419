48779bececa31811b0032c02c0f62c949ef27eb0
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-02-13 23:10:52 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -5,3 +5,8 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def down
   end
 end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
